bcc337b6cab3916a0dd2b96eee5156a5e90cc367
Alyssa Roose <roose.alyssa@gmail.com>
2019-01-13 18:26:33 +0100

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DB_PATH: './data/json/',
+  TYPE_STRING: Symbol('type__string'),
+  TYPE_NUMBER: Symbol('type__number'),
+  TYPE_ARRAY: Symbol('type__array'),
+  TYPE_OBJECT: Symbol('type__object'),
+  TYPE_BOOLEAN: Symbol('type__boolean'),
+  TYPE_FUNCTION: Symbol('type__function'),
+};
