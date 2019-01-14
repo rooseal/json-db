b6abcd576f10f7d381a5acd11807b2544e98414c
Alyssa Roose <roose.alyssa@gmail.com>
2019-01-14 08:17:58 +0100

    Add modelname and the update function to the createModelCRUD

--- src/createModelCrud.js
+++ src/createModelCrud.js
@@ -3,58 +3,99 @@ const error = require('./utils/error');
 const writeFile = require('./helpers/promised/writeFile');
 const getResource = require('./helpers/getResource');
 const validateModel = require('./validation/validateModel');
+const validateUpdate = require('./validation/validateUpdate');
 
 module.exports = ({
+  modelName = requiredParam('createModelCRUD', 'modelName', true),
   resourcePath = requiredParam('createModelCRUD', 'resourcePath', true),
   fields = requiredParam('createModelCRUD', 'fields', true),
 }) => ({
   async create(data) {
     // Validate the data
-    const validationStatus = validateModel(data, fields);
-
-    if (validationStatus.error) {
-      return error(validationStatus.error, {});
+    try {
+      validateModel(data, fields);
+    } catch (err) {
+      throw Error(`[${modelName}] Error while creating new resource. ${err}`);
     }
 
-    // Get the collection from memory
+    // if (validationStatus.error) {
+    //   return error(validationStatus.error, {});
+    // }
 
+    // Get the collection from memory
     let collection;
 
     try {
       collection = await getResource(resourcePath);
     } catch (errMessage) {
-      return error(errMessage, []);
+      throw Error(
+        `[${modelName}] Error while creating new resource. ${errMessage}`,
+      );
     }
 
     // Save the new collection to memory
     collection.push(data);
-    const { err } = await writeFile(resourcePath, collection);
 
-    if (err) {
-      return error(err, {});
+    try {
+      await writeFile(resourcePath, JSON.stringify(collection, null, 2));
+    } catch (err) {
+      throw Error(`[${modelName}] Error while creating new resource. ${err}`);
     }
 
-    return {
-      error: false,
-      data,
-    };
+    return data;
   },
 
-  async read(query = () => {}) {
+  async read(query = () => true) {
     let collection;
 
     try {
       collection = await getResource(resourcePath);
     } catch (errMessage) {
-      return error(errMessage, []);
+      throw Error(
+        `[${modelName}] Error while reading from resource. Couldn't load collection from database. ${errMessage}`,
+      );
     }
 
     const results = collection.filter(query);
 
-    return {
-      error: false,
+    return results;
+  },
 
-      data: results,
-    };
+  async update(changes, query = () => false, overwrite = false) {
+    // Validate the data
+    try {
+      validateUpdate(changes, fields);
+    } catch (err) {
+      throw Error(`[${modelName}] Error while updating resource ${err}`);
+    }
+
+    // if (validationStatus.error) {
+    //   return error(validationStatus.error, {});
+    // }
+
+    // Get the collection from memory
+    let collection;
+
+    try {
+      collection = await getResource(resourcePath);
+    } catch (errMessage) {
+      throw Error(`[${modelName}] Error while updating resource ${errMessage}`);
+    }
+
+    const updatedCollection = collection.map(currentItem =>
+      query(currentItem)
+        ? overwrite
+          ? changes
+          : { ...currentItem, ...changes }
+        : currentItem,
+    );
+
+    try {
+      await writeFile(resourcePath, JSON.stringify(updatedCollection, null, 2));
+    } catch (err) {
+      throw Error(`[${modelName}] Error while updating resource ${err}`);
+    }
+
+    return updatedCollection;
   },
 });
